ae731b2416c51cc284449c40e7124c6befd5e4a9
t.puzanov <tamirpuzanov@gmail.com>
2024-07-04 04:25:28 +0700

    Fix imports

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import { Program } from "./Program";
 import { Package, ConnectednessError, InferiorityError, OverflowError } from "./Package";
 import { Command, CommandType, CommandList } from "./Command";
 import { DefinedCommands, defined_command_fn } from "./DefinedCommands";
+import { Interpreter, HaltError } from "./Interpreter";
 import { Tape } from "./Tape";
 
-export { Parser, ParseError, Program, Tape };
+export { Interpreter, HaltError, Parser, ParseError, Program, Tape };
 export { Package, ConnectednessError, InferiorityError, OverflowError };
 export { Command, CommandList, CommandType };
 export { DefinedCommands, defined_command_fn }
